bc79fe954ad65978af05604853eef6555c57b767
Adil0710 <padil2246@gmail.com>
2025-03-01 17:26:37 +0530

    expense store

--- src/lib/store/expensesStore.ts
+++ src/lib/store/expensesStore.ts
@@ -1,27 +1,27 @@
 import axios from "axios";
 import { create } from "zustand";
 
-
 interface Expenses {
-    expenses : [
-        {
-            id: string
-            userId: string,
-            categoryId: string,
-            amount: number,
-            isOverLimit: boolean,
-            description: string,
-            createdAt: string,
-            updatedAt: string,
-            category:{
-                name: string,
-                limit: number
-            },
-            totalSpent: number
-        }
-    ]
+  expenses: [
+    {
+      id: string;
+      userId: string;
+      categoryId: string;
+      amount: number;
+      isOverLimit: boolean;
+      description: string;
+      createdAt: string;
+      updatedAt: string;
+      category: {
+        name: string;
+        limit: number;
+      };
+      totalSpent: number;
+    }
+  ];
 }
 
 interface ExpensesState {
-    expense : Expenses | null,
+  expenses: Expenses | null;
+  expensesLoading: boolean;
 }
