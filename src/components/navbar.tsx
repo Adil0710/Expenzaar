32830dc09a5a8a8695909680cc6a281cda43b5f2
Adil0710 <padil2246@gmail.com>
2025-03-19 23:28:37 +0530

    added dashboard with sidebar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,26 +1,32 @@
-"use client"
-import React from 'react'
-import LoaderLogo from './loader'
-import { ModeToggle } from '@/app/components/mode-toggle'
-import Link from 'next/link'
-import { Button } from './ui/button'
-import { signOut, useSession } from 'next-auth/react'
+"use client";
+import React from "react";
+import LoaderLogo from "./loader";
+import { ModeToggle } from "@/app/components/mode-toggle";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { signOut, useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
 function Navbar() {
-  const session = useSession()
+  const session = useSession();
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/dashboard")) {
+    return <div></div>;
+  }
+
   return (
-  <nav className=' flex justify-between items-center py-2 px-5 bg-white/20 backdrop-blur-xs text-black fixed w-full  dark:bg-black/20 dark:text-white'>
-    <LoaderLogo/>
-    <div>
-    <Link href='/signin' className=''>Sign In</Link>
-    <ModeToggle/>
-    {
-      session.data  &&  <Button onClick={() => signOut()}>Log Out</Button>
-    }
-   
-    </div>
-  </nav>
-  )
+    <nav className=" flex justify-between items-center py-2 px-5 bg-white/20 backdrop-blur-xs text-black fixed w-full  dark:bg-black/20 dark:text-white">
+      <LoaderLogo />
+      <div>
+        <Link href="/signin" className="">
+          Sign In
+        </Link>
+        <ModeToggle />
+        {session.data && <Button onClick={() => signOut()}>Log Out</Button>}
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
