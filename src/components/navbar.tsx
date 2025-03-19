3a26a7b488c50fbcbd8b8d58c15816063c0fddf0
Adil0710 <padil2246@gmail.com>
2025-03-19 23:10:25 +0530

    added dashboard with sidebar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ import { signOut, useSession } from 'next-auth/react'
 function Navbar() {
   const session = useSession()
   return (
-  <nav className=' flex justify-between items-center py-2 px-5 bg-white/20 backdrop-blur-xs text-black fixed w-full  dark:bg-black dark:text-white'>
+  <nav className=' flex justify-between items-center py-2 px-5 bg-white/20 backdrop-blur-xs text-black fixed w-full  dark:bg-black/20 dark:text-white'>
     <LoaderLogo/>
     <div>
     <Link href='/signin' className=''>Sign In</Link>
