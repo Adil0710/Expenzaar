46377a92b0430f7f5a9fdbc1238b5bc32db4f41f
Adil0710 <padil2246@gmail.com>
2025-04-08 18:26:41 +0530

    added password reset

--- src/app/(pages)/(auth)/signup/page.tsx
+++ src/app/(pages)/(auth)/signup/page.tsx
@@ -4,7 +4,6 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { useForm } from "react-hook-form";
